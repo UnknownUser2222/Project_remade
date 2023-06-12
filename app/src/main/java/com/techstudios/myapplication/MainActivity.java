f8da15cb17e35558cf188488bbac2b9661ecd5aa
Muhammad Ayan <userunamed981@gmail.com>
2023-06-12 12:11:50 +0600

    Initial commit

---
+++ app/src/main/java/com/techstudios/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.techstudios.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText input;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        input = (EditText) findViewById(R.id.input1);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello, My name is "+input.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
